1e58df895220c153439de89edf83d7c15bde3606
Daniel Zacarias <daniel.santos@bandtec.com.br>
2022-09-11 15:56:09 -0300

    Sensor generate and create temperature data

--- sensors/ht11.py
+++ sensors/ht11.py
@@ -1,19 +1,26 @@
-import time
+import asyncio
 from datetime import datetime
 from sensors.sensor import Sensor
 import numpy as np
+import uuid
 
 
 class HT11(Sensor):
-    def generate(self):
+    async def generate(self):
         print("Simulando dados de temperatura")
         base = self.get_random_number()
         while True:
             data_temperature = np.random.uniform(low=base - 2, high=base + 2)
-            data = np.random.choice([np.round(data_temperature, 1), None], p=[0.9, 0.1])
+            self.data = np.random.choice([np.round(data_temperature, 1), None], p=[0.9, 0.1])
             date_now = datetime.now()
-            print(f"{date_now.strftime('%m/%d/%Y %H:%M:%S')} - {data} Cº")
-            time.sleep(1)
+            self.data = {
+                "temperature": self.data,
+                "date_register": date_now.strftime('%m/%d/%Y %H:%M:%S'),
+                "message_id": str(uuid.uuid4())
+            }
+            print(f"{self.data['date_register']} - {self.data['temperature']} Cº")
+            await self.send_data()
+            await asyncio.sleep(1)
 
     def get_random_number(self):
         return np.random.uniform(low=30.5, high=45.5)
--- sensors/sensor.py
+++ sensors/sensor.py
@@ -1,8 +1,27 @@
 import abc
+import json
 
+from azure.iot.device import Message
+from azure.iot.hub import IoTHubRegistryManager
+
+from constants import CONNECTION_STRING, MESSAGE_COUNT, MSG_TXT
+from azure.iot.device.aio import IoTHubDeviceClient
 
 class Sensor(abc.ABC):
+    def __init__(self):
+        self.data = 0
+        self.device = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
     @abc.abstractmethod
-    def generate(self):
+    async def generate(self):
         pass
+
+    async def send_data(self):
+        try:
+            # Create IoTHubRegistryManager
+            message = Message(json.dumps(self.data))
+            await self.device.send_message(message)
+            print("Enviando mensagem para Nuvem")
+        except Exception as ex:
+            print("Unexpected error " + ex)
+            return
